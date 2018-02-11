0fcd697682be397dd696e39cdb5f5d2d7ea2bf5f
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-11 11:23:49 +0000

    Reverse String

--- StringInReverse/Program.cs
+++ StringInReverse/Program.cs
@@ -10,6 +10,35 @@ namespace StringInReverse
     {
         static void Main(string[] args)
         {
+            string str; /* Declares a string of size 100 */
+            int i,l;
+
+            Console.Write("\n\nInput a string :\n");
+            Console.Write("---------------------------------\n");
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            
+
+            char[] array = str.ToCharArray();
+            l = array.Length;
+            for (i = l-1; i >=0; i--)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+            /*   using wjile loop  
+               while (l <= str.Length - 1)
+                 {
+                     Console.Write("{0} ", str[l]);
+                     l++;
+                 }**/
+
+
+            Console.Write("\n\n");
+
+
+
+            Console.ReadLine();
         }
     }
 }
